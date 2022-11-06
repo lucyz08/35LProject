bb61504ec74dfa07d7bd181a8c15c27c4d58c879
classjek <jekoniak5741@ucla.edu>
2022-11-06 15:20:41 -0800

    Delete excess files and create router to allow page changes
    I deleted a bunch of the default files that we won't use in our project.
    I also edited the rest of the files to allow for page changes. I did
    this using react-router-node

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Navbar"
+import Home from "./Home"
+import About from "./About"
+import Friends from "./Friends"
+import Profile from "./Profile"
+import { Routes, Route } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <h1>Arpeggio</h1>
+  <>
+    <Navbar />
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/friends" element={<Friends />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </div>
-  );
+  </>
+  )
 }
 
 export default App;
