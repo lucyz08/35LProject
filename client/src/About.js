e0b3fd50996630236ecaac11bd6142f3c9e6be4b
classjek <jekoniak5741@ucla.edu>
2022-11-06 15:33:22 -0800

    New files for router navigation
    Here I added the files necessary for file navigation. These files will
    be the body of our app but I just added the skeletons of them
    
    e Please enter the commit message for your changes. Lines starting

---
+++ client/src/About.js
@@ -0,0 +1,17 @@
+export default function About(){
+    return (
+        <>
+        <div>
+            <h1 className="frontend">
+                Frontend:
+            </h1>
+
+        </div>
+        <div>
+            <h1 className="backend">
+                Backend:
+            </h1>
+        </div>
+        </>
+    )
+}
---
+++ client/src/Navbar.js
@@ -0,0 +1,25 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom"
+
+export default function Navbar() {
+    return <nav className="nav">
+        <Link to="/" className="site-title">
+            Arpeggio
+        </Link>
+        <ul>
+            <Checkact to="/profile">Profile</Checkact>
+            <Checkact to="/friends">Friends</Checkact>
+            <Checkact to="/about">About</Checkact>
+        </ul>
+    </nav>
+}
+
+function Checkact({to, children, ...props}) {
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+
+    return (
+        <li className={isActive ? "active" : ""}>
+            <Link to={to}{...props}>{children}</Link>
+        </li>
+    )
+}
