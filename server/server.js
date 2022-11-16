fd0d76d6c389e12ca52f6b5cd084ddd3c9eae6e2
Gene Bordegaray <gene.bordegaray21@gmail.com>
2022-11-16 09:45:21 -0800

    All the json and node_modules that my program is running on since I had to npm install some new things while working.
    Also in server.js I was getting some weird access warning but the
    addition of useNewURLParser: true, useUnifiedTopology: true seemed to
    have fixed it

--- server/server.js
+++ server/server.js
@@ -16,6 +16,6 @@ app.use('/data', postRoutes);
 const CONNECTION_URL = 'mongodb+srv://GeneBordegaray:Gene2003@35lproject.tnn1kyn.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL)
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:5000`)))
   .catch((error) => console.log(`failed connection`));
