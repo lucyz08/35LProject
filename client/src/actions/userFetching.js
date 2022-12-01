8708cc850266be1d9c10f002b634b699911392ac
Gene Bordegaray <gene.bordegaray21@gmail.com>
2022-11-30 18:18:38 -0800

    Fixing black screen for profile and home pages for starting app

--- client/src/actions/userFetching.js
+++ client/src/actions/userFetching.js
@@ -4,6 +4,13 @@ export const signin = (post) =>async (dispatch) => {
     try {
         const {data} = await api.signIn(post);
         dispatch({type: 'AUTH', payload: data});
+
+        const currUsername = JSON.parse(localStorage.getItem('profile'))
+        const you = currUsername.result.username
+        const filter = {"username": you}
+        const {userData} = await api.fetchUser(filter)
+        dispatch( {type:'FRIEND', payload: userData})
+
         window.location.reload();
         window.location.assign("http://localhost:3000/welcome");
     } catch (error) {
@@ -16,6 +23,13 @@ export const signup = (post) => async (dispatch) => {
     try {
         const {data} = await api.signUp(post);
         dispatch({type: 'AUTH', payload: data});
+
+        const currUsername = JSON.parse(localStorage.getItem('profile'))
+        const you = currUsername.result.username
+        const filter = {"username": you}
+        const {userData} = await api.fetchUser(filter)
+        dispatch( {type:'FRIEND', payload: userData})
+
         window.location.reload();
         window.location.assign("http://localhost:3000/welcome");
     } catch (error) {
